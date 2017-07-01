fdd2f0762aaeedea1cec59efc95dfec4372cf4b0
lowery <hello@davidlowery.net>
2017-07-01 18:32:16 -0400

    Testing deployment via heroku and mongolab addon

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 
     // Database
     mongodb: {
-        url: 'mongodb://localhost:27017/efg',
+        url: 'mongodb://heroku_vbfhqhmh:hj53c9mv6gobpuuran7ig7ffa7@ds145892.mlab.com:45892/heroku_vbfhqhmh'
     },
 
     // Image server
